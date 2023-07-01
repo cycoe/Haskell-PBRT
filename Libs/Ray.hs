78514271862d0a37f9341f97e766643e38382de9
cycoe <cycoejoo@163.com>
2023-07-01 15:58:59 +0800

    [ADDDE] Add Ray implement

---
+++ Libs/Ray.hs
@@ -0,0 +1,10 @@
+module Libs.Ray where
+
+import Libs.Vector (Vector3, (.+.), (*.))
+
+data Ray = Ray { getOrigin :: Vector3 Float
+               , getDirection :: Vector3 Float
+               } deriving Show
+
+transport :: Ray -> Float -> Vector3 Float
+transport (Ray o d) t = o .+. (t *. d)
