ed3f5bf62d84734cfb428765a7316ccc6e02244d
cycoe <cycoejoo@163.com>
2023-07-07 23:41:48 +0800

    [ADDED] Add command line arguments parser

---
+++ Libs/Configs.hs
@@ -0,0 +1,6 @@
+module Libs.Configs
+  ( Configs(..)
+  ) where
+
+data Configs = Configs { quickCheck :: Bool
+                       } deriving Show
--- Main.hs
+++ Main.hs
@@ -1,4 +1,22 @@
 module Main (main) where
 
+import Options.Applicative
+import Libs.Configs (Configs(..))
+
+configP :: Parser Configs
+configP = Configs
+  <$> switch ( long "quick-check"
+            <> short 'q'
+            <> help "Quick check rendering result with low resolutions and spp"
+            <> showDefault
+             )
+
 main :: IO ()
-main = undefined
+main = render =<< execParser opts where
+  opts = info (configP <**> helper)
+    ( fullDesc
+   <> header "Physical-based ray tracing, but with Haskell."
+    )
+
+render :: Configs -> IO ()
+render configs = print configs
