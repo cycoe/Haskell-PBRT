4d870ec4b656c3fe4757cec96e68fe8ce30db801
cycoe <cycoejoo@163.com>
2023-07-29 21:22:19 +0800

    [REFACTOR] Modify Intersection comments

--- Libs/Intersection.hs
+++ Libs/Intersection.hs
@@ -2,11 +2,10 @@ module Libs.Intersection where
 
 import Libs.Vector (Vector3)
 
--- Intersection has two possible constructors. One stands for no intersection,
--- the other stands for intersecting with an object
+-- | Intersection record the information of intersecting with an object
 data Intersection o = Intersection
-                      { getCoordinate :: Vector3 Float
-                      , getNormal :: Vector3 Float
-                      , getObject :: o
+                      { getCoordinate :: Vector3 Float  -- ^ Intersecting coordinate
+                      , getNormal :: Vector3 Float      -- ^ Normal of intersection
+                      , getObject :: o                  -- ^ Object that intersection on
                       }
                     deriving Show
